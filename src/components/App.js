5352a017b027ca7b87ee24b5ebfb8b3a44956d59
Olya Yakymovych <msolya@outlook.com>
2018-01-19 09:43:21 +0200

    created the react app

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
